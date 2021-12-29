6a174732822a3f8bcd1d7aa0bb520d36051a2de2
codebysachin <codebysachin@gmail.com>
2021-12-29 21:04:09 +0000

    Initial Commit

---
+++ src/main/java/com/coffeeatwork/springusermx/SpringUserMxApplication.java
@@ -0,0 +1,13 @@
+package com.coffeeatwork.springusermx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserMxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUserMxApplication.class, args);
+	}
+
+}
